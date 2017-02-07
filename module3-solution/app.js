62a2661337d64fb0a899520e806102cf30226b6e
Vasyl Novosad <novosadvasiliy@gmail.com>
2017-02-07 23:43:00 +0200

    added module3 solution

---
+++ module3-solution/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Fr1end on 07.02.2017.
+ */
+(function () {
+    'use strict';
+
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService);
+
+})();
