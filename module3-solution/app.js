891f96a91c7a8d3efc444ca80104da0713c2f183
Vasyl Novosad <novosadvasiliy@gmail.com>
2017-02-09 23:46:09 +0200

    added components

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -6,6 +6,74 @@
 
     angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController)
-        .service('MenuSearchService', MenuSearchService);
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', FoundItems)
+        .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+    function FoundItems() {
+        var ddo = {
+            templateUrl: 'foundItems.html',
+            scope: {
+                foundItems: '<',
+                onRemove: '&'
+            }
+        };
+
+        return ddo;
+    }
+
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService($http, ApiBasePath) {
+        var service = this;
+
+        service.getMatchedMenuItems = function (searchTerm) {
+            var response = $http({
+                method: "GET",
+                url: (ApiBasePath + "/menu_items.json")
+            }).then(function(result) {
+                var foundItems = [];
+                angular.forEach(result.data, function(value, key){
+                    if (value.description.indexOf(searchTerm) != -1) {
+                        foundItems.push(value);
+                    }
+                });
+                return foundItems;
+            });
+
+            return response;
+        };
+
+    }
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var menu = this;
+        menu.searchTerm = '';
+
+        var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+
+        promise.then(function (response) {
+            menu.found = response;
+        })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+        menu.getMenuItems = function () {
+            var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+
+            promise.then(function (response) {
+                console.log(response.data);
+            })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        };
+
+        menu.removeItem = function (itemIndex) {
+            menu.found.splice(itemIndex, 1);
+        }
+    }
+
 
 })();
